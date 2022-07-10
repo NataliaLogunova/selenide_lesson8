66daefa2661ee73391c0e6b421e85a3d3fce7dbc
NataliaLogunova <natali.elle@mail.ru>
2022-07-11 00:51:47 +0500

    refactoring .gitignore

--- src/test/java/org/nataliya/SoftAssertionsTest.java
+++ src/test/java/org/nataliya/SoftAssertionsTest.java
@@ -2,7 +2,6 @@ package org.nataliya;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
